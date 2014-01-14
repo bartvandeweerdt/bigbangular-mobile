4a0070982e4015459eb9d67eaebffef4cccb77b2
Bart Vandeweerdt <bart.vandeweerdt@ida-mediafoundry.be>
2014-01-14 13:55:01 +0100

    grunt clean now deletes complete dist folder.

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,8 +93,7 @@ module.exports = function (grunt) {
           dot: true,
           src: [
             '.tmp',
-            '<%= yeoman.dist %>/*',
-            '!<%= yeoman.dist %>/.git*'
+            '<%= yeoman.dist %>'
           ]
         }]
       },
